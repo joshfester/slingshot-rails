89d5f3390d25bcd001349d7f2ec35de342999c86
Josh Fester <joshfester@gmail.com>
2022-10-02 16:15:35 +0000

    create ApplicationService

---
+++ app/services/application_service.rb
@@ -0,0 +1,32 @@
+class ApplicationService
+
+  attr_reader :result
+  attr_reader :fail_message
+
+  def self.call(**args)
+    new(**args).tap do |service|
+      service.instance_variable_set(
+        "@result",
+        service.call
+      )
+    end
+  end
+
+  def call
+    raise NotImplementedError
+  end
+
+  def success?
+    !failure?
+  end
+
+  def failure?
+    @failure || false
+  end
+
+  def fail!(message = '')
+    @fail_message = message
+    @failure = true
+  end
+
+end
