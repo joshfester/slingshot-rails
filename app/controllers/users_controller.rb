5a5304b06121266dbdd14cc69d066972c2fff24f
Josh Fester <joshfester@gmail.com>
2022-10-09 05:07:48 +0000

    design change password page. profile form works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,24 @@ class UsersController < ApplicationController
   end
 
   def update 
+    current_user.assign_attributes(resource_params)
 
+    if current_user.save
+      flash[:success] = 'Profile saved'
+      redirect_back_or_to profile_path
+    else
+      flash[:danger] = 'Error saving profile'
+      redirect_back_or_to profile_path
+    end
+  end
+
+  private
+
+  def resource_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name
+    )
   end
 
 end
